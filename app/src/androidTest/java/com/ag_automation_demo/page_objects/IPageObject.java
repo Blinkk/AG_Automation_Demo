45d25462c177aaa9a367f8a67895603ba1d51b41
Blinkk <austin.ivicevic@outlook.com>
2018-01-01 18:59:30 -0800

    Move old fragment layout to activity; minor additional clean-up

--- app/src/androidTest/java/com/ag_automation_demo/page_objects/IPageObject.java
+++ app/src/androidTest/java/com/ag_automation_demo/page_objects/IPageObject.java
@@ -1,10 +1,10 @@
 package com.ag_automation_demo.page_objects;
 
 /**
- * Created by Austin on 1/1/2018.
+ * Define common page object functionality
  */
 
-public interface IPageObject
+interface IPageObject
 {
     // This is the only function that should have an assertion within a page object
     // as a quick way to check that a page is loaded after a set of actions
